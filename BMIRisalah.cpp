46f03197e8878e815b06552c87fb8b463c31acfb
risalahlele87-coder <risalahleles87@gmail.com>
2026-03-09 15:10:13 +0700

    Menambahkan prosedur input data

--- BMIRisalah.cpp
+++ BMIRisalah.cpp
@@ -1,3 +1,13 @@
 #include <iostream>
 using namespace std;
 
+// Prosedur input data
+void InputData(float &berat, float &tinggi)
+{
+    cout << "--- Selamat Datang Mahasiswa Kelas A 2025 ---" << endl;
+    cout << "Masukkan berat badan (kg): ";
+    cin >> berat;
+    cout << "Masukkan tinggi badan (meter): ";
+    cin >> tinggi;
+}
+
