582752b77c7b37211369c413aaa92fe47a398ffc
risalahlele87-coder <risalahleles87@gmail.com>
2026-03-09 15:12:11 +0700

    Menambahkan fungsi status BMI

--- BMIRisalah.cpp
+++ BMIRisalah.cpp
@@ -17,3 +17,16 @@ float HitungBMI(float berat, float tinggi)
     return berat / (tinggi * tinggi);
 }
 
+// Fungsi menentukan status BMI
+string StatusBMI(float bmi)
+{
+    if (bmi < 18.5)
+        return "Berat Badan Kurang";
+    else if (bmi < 25)
+        return "Berat Badan Normal";
+    else if (bmi < 30)
+        return "Berat Badan Kelebihan";
+    else
+        return "Obesitas";
+}
+
